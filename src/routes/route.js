e9fec7e99dc4f9d853044bd462a494f8c585ce1e
neha kumari <nk5307405@gmail.com>
2022-12-19 17:51:48 +0530

    add file

---
+++ src/routes/route.js
@@ -0,0 +1,16 @@
+const { Router } = require("express")
+const express=require("express")
+const router=express.Router()
+const userController=require("../controllers/userController")
+const mid=require("../middleware/auth")
+const aws=require("../controllers/aws")
+//=============user api==================//
+router.post("/register",userController.createUser);
+router.post("/login",userController.loginuser)
+router.get("/user/:userId/profile",mid.Authentication,userController.getUser)
+router.post('/write-file-aws',aws.getImage)
+
+
+
+
+module.exports=router
